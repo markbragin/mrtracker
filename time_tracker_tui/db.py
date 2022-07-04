36f10d3edb91df3d9f26d66ce4b3ce824bb525d8
delbik <m4rk.brag1n@gmail.com>
2022-07-04 14:44:20 +0300

    refactoring

--- time_tracker_tui/db.py
+++ time_tracker_tui/db.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 import sqlite3
 from platformdirs import user_data_dir
 
-from definition import ROOT_PKG_DIR
+from .definition import ROOT_PKG_DIR
 
 
 DB_NAME = "test.db"
@@ -24,33 +24,33 @@ def _check_db_exists() -> bool:
 
 def _create_total_table() -> None:
     cur.execute(
-            "CREATE TEMP TABLE tt0 AS "
-            "SELECT s.task_id, sum(s.time) as total "
-            "FROM sessions s "
-            "GROUP BY s.task_id "
-            "ORDER BY s.id DESC"
+        "CREATE TEMP TABLE tt0 AS "
+        "SELECT s.task_id, sum(s.time) as total "
+        "FROM sessions s "
+        "GROUP BY s.task_id "
+        "ORDER BY s.id DESC"
     )
     conn.commit()
 
 
 def _create_today_table() -> None:
     cur.execute(
-            "CREATE TEMP TABLE tt1 AS "
-            "SELECT s.task_id, sum(s.time) as today "
-            "FROM sessions s "
-            "WHERE s.date = date('now', 'localtime') "
-            "GROUP BY s.task_id"
+        "CREATE TEMP TABLE tt1 AS "
+        "SELECT s.task_id, sum(s.time) as today "
+        "FROM sessions s "
+        "WHERE s.date = date('now', 'localtime') "
+        "GROUP BY s.task_id"
     )
     conn.commit()
 
 
 def _create_month_table() -> None:
     cur.execute(
-            "CREATE TEMP TABLE tt2 AS "
-            "SELECT s.task_id, sum(s.time) as month "
-            "FROM sessions s "
-            "WHERE s.date LIKE strftime('%Y-%m-%%', 'now') "
-            "GROUP BY s.task_id"
+        "CREATE TEMP TABLE tt2 AS "
+        "SELECT s.task_id, sum(s.time) as month "
+        "FROM sessions s "
+        "WHERE s.date LIKE strftime('%Y-%m-%%', 'now') "
+        "GROUP BY s.task_id"
     )
     conn.commit()
 
@@ -78,15 +78,15 @@ def fetch_full_info() -> List[Tuple]:
     cur.execute(
         "SELECT tt0.task_id, l.parent_id, "
         "t.name, tt1.today, tt2.month, tt0.total "
-        "FROM tt0 "
+        "FROM tasks t "
+        "LEFT OUTER JOIN tt0 "
+        "ON t.id = tt0.task_id "
         "LEFT OUTER JOIN tt1 "
-        "ON tt0.task_id = tt1.task_id "
+        "ON t.id = tt1.task_id "
         "LEFT OUTER JOIN tt2 "
-        "ON tt0.task_id = tt2.task_id "
+        "ON t.id = tt2.task_id "
         "LEFT OUTER JOIN links l "
-        "ON tt0.task_id = l.task_id left "
-        "LEFT OUTER JOIN tasks t "
-        "ON tt0.task_id = t.id "
+        "ON t.id = l.task_id "
     )
     result = cur.fetchall()
     _drop_temp_tables()
