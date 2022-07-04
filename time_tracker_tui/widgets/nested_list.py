2d5fd6c95735719fa5377368f4fd646887a14125
delbik <m4rk.brag1n@gmail.com>
2022-07-04 15:21:43 +0300

    refactoring

--- time_tracker_tui/widgets/nested_list.py
+++ time_tracker_tui/widgets/nested_list.py
@@ -1,7 +1,6 @@
-from rich.console import RenderableType
 from rich.padding import PaddingDimensions
-from rich.text import TextType, Text
-from textual.widgets import TreeControl
+from rich.text import Text, TextType
+from textual.widgets import NodeID, TreeControl
 
 
 class NestedList(TreeControl):
@@ -13,26 +12,31 @@ class NestedList(TreeControl):
         padding: PaddingDimensions = (1, 1),
     ) -> None:
         super().__init__(label, data, name=name, padding=padding)
-        self._tree.hide_root = True
+        # self._tree.hide_root = True
         self.show_cursor = True
 
-    async def add_child(self, label: TextType, data: RenderableType) -> None:
+    async def add_child(self, data: tuple, label: TextType = "") -> None:
         await self.add(self.cursor, label=label, data=data)
         await self.nodes[self.cursor].expand()
-        await self.go_to_latest_child()
+        await self._cur_to_latest_child()
 
-    async def add_sibling(self, label: TextType, data: RenderableType) -> None:
+    async def add_sibling(self, data: tuple, label: TextType = "") -> None:
         node = self.nodes[self.cursor]
         parent = node.parent
         if parent:
             await self.add(parent.id, label=label, data=data)
+            await self._cur_to_latest_child()
+
+    async def add_root_child(self, data: tuple, label: TextType = "") -> None:
+        await self.add(self.root.id, label=label, data=data)
+        await self._cur_to_latest_child()
 
     async def remove_node(self) -> None:
         node = self.nodes[self.cursor]
         parent = node.parent
         if parent:
             if node.next_sibling:
-                await self.go_to_next_sibling()
+                await self._cur_to_next_sibling()
             else:
                 await self.cursor_up()
             parent.children.remove(node)
@@ -40,71 +44,23 @@ class NestedList(TreeControl):
 
         self.refresh(layout=True)
 
-    async def go_to_latest_child(self) -> None:
-        self._remove_hl()
-        await self._cur_to_latest_child()
-        self._highlight()
-
     async def _cur_to_latest_child(self) -> None:
         node_id = self.id
         while self.cursor != node_id:
             await self.cursor_down()
 
-    async def go_to_parent(self) -> None:
-        self._remove_hl()
-        await self._cur_to_parent()
-        self._highlight()
-
     async def _cur_to_parent(self) -> None:
-        node = self.nodes[self.cursor]
-        parent = node.parent
-        if parent is self.root:
-            return
-
+        parent = self.nodes[self.cursor].parent
         if parent:
             while self.cursor != parent.id:
                 await self.cursor_up()
 
-    async def go_to_next_sibling(self) -> None:
-        self._remove_hl()
-        await self._cur_to_next_sibling()
-        self._highlight()
-
     async def _cur_to_next_sibling(self) -> None:
-        node = self.nodes[self.cursor]
-        sibling = node.next_sibling
+        sibling = self.nodes[self.cursor].next_sibling
         if sibling:
             while self.cursor != sibling.id:
                 await self.cursor_down()
 
-    async def cur_to_root(self) -> None:
-        while self.cursor != self.root.id:
-            await self.cursor_up()
-
-    async def go_down(self) -> None:
-        self._remove_hl()
-        await self.cursor_down()
-        self._highlight()
-
-    async def go_up(self) -> None:
-        node = self.nodes[self.cursor]
-        if node.previous_node is self.root:
-            return
-
-        self._remove_hl()
-        await self.cursor_up()
-        self._highlight()
-
-    def _highlight(self) -> None:
-        node = self.nodes[self.cursor]
-        if isinstance(node.label, str):
-            node.label = Text(node.label, style="reverse")
-        elif isinstance(node.label, Text):
-            node.label.style = "reverse"
-
-    def _remove_hl(self) -> None:
-        node = self.nodes[self.cursor]
-        if isinstance(node.label, str):
-            node.label = Text(node.label, style="")
-        elif isinstance(node.label, Text):
-            node.label.style = ""
+    async def _cur_to_root(self) -> None:
+        self.cursor = NodeID(0)
+        self.cursor_line = 0
