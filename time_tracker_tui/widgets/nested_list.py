61d3c51e10607949cfaf64c31547462ca75dfc44
delbik <m4rk.brag1n@gmail.com>
2022-07-08 15:59:48 +0300

    refactoring, added remove_childless_node()

--- time_tracker_tui/widgets/nested_list.py
+++ time_tracker_tui/widgets/nested_list.py
@@ -7,42 +7,38 @@ class NestedList(TreeControl):
     def __init__(
         self,
         label: TextType,
-        data: tuple,
+        data,
         name: str | None = None,
         padding: PaddingDimensions = (1, 1),
     ) -> None:
         super().__init__(label, data, name=name, padding=padding)
-        # self._tree.hide_root = True
         self.show_cursor = True
 
-    async def add_child(self, data: tuple, label: TextType = "") -> None:
+    async def add_child(self, label: TextType, data) -> None:
         await self.add(self.cursor, label=label, data=data)
         await self.nodes[self.cursor].expand()
         await self._cur_to_latest_child()
 
-    async def add_sibling(self, data: tuple, label: TextType = "") -> None:
+    async def add_sibling(self, label: TextType, data) -> None:
         node = self.nodes[self.cursor]
-        parent = node.parent
-        if parent:
-            await self.add(parent.id, label=label, data=data)
-            await self._cur_to_latest_child()
+        if node is self.root:
+            return
+        await self._cur_to_parent()
+        await self.add_child(label, data)
 
-    async def add_root_child(self, data: tuple, label: TextType = "") -> None:
-        await self.add(self.root.id, label=label, data=data)
-        await self._cur_to_latest_child()
+    async def add_root_child(self, label: TextType, data) -> None:
+        await self._cur_to_root()
+        await self.add_child(label, data)
 
-    async def remove_node(self) -> None:
+    async def remove_childless_node(self) -> None:
+        if self.cursor == self.root.id:
+            return
+
         node = self.nodes[self.cursor]
-        parent = node.parent
-        if parent:
-            if node.next_sibling:
-                await self._cur_to_next_sibling()
-            else:
-                await self.cursor_up()
-            parent.children.remove(node)
-            parent.tree.children.remove(node.tree)
-
-        self.refresh(layout=True)
+        await self.cursor_up()
+        node.parent.tree.children.remove(node.tree)
+        node.parent.children.remove(node)
+        del self.nodes[node.id]
 
     async def _cur_to_latest_child(self) -> None:
         node_id = self.id
