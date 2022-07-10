a296b83267044fd2a3484c756020bb18b47460d5
delbik <m4rk.brag1n@gmail.com>
2022-07-10 14:10:55 +0300

    info.py -> in_app_logger.py

---
+++ time_tracker_tui/widgets/in_app_logger.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from rich.align import Align
+from rich.console import RenderableType
+from rich.panel import Panel
+from textual.reactive import Reactive
+from textual.widget import Widget
+
+from ..__init__ import __version__
+from ..styles import styles
+
+
+class Logger(Widget, can_focus=False):
+
+    _content: Reactive[str] = Reactive("")
+    _error: Reactive[bool] = Reactive(False)
+
+    def __init__(
+        self,
+        name: str | None = "Logger",
+        msg: str = f"Time tracker v{__version__}\n[blue]ctrl+h[/] - show help",
+    ) -> None:
+        super().__init__(name=name)
+        self._content = msg
+
+    @property
+    def content(self) -> str:
+        return self._content
+
+    @content.setter
+    def content(self, new_content) -> None:
+        self._content = new_content
+
+    @property
+    def error(self) -> bool:
+        return self._error
+
+    @error.setter
+    def error(self, upd) -> None:
+        self._error = upd
+
+    def update(self, msg: str, error: bool = False):
+        self._error = error
+        self._content = msg
+
+    def render(self) -> RenderableType:
+        if self._error:
+            self.style = styles["LOGGER_ERROR"]
+        else:
+            self.style = styles["LOGGER"]
+
+        self.panel = Panel(
+            Align.center(self.content, vertical="middle", style="white"),
+            border_style=self.style,
+            expand=True,
+        )
+        return self.panel
+
+
+ialogger = Logger()
