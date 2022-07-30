290810c902ef794ac1fff981be31eed78ab746b6
delbik <m4rk.brag1n@gmail.com>
2022-07-30 20:31:46 +0300

    refactoring

--- mrtracker/widgets/entry.py
+++ mrtracker/widgets/entry.py
@@ -1,44 +1,53 @@
-from enum import Enum
-
 from textual.reactive import events
 
-from ..timetuple import TimeTuple
 from .text_input import TextInput
-
-
-class EntryType(Enum):
-    FOLDER = "f"
-    TASK = "t"
 
 
 class Entry(TextInput):
 
-    task_id: int
-    parent_id: int
-    name: str
-    etype: EntryType
-    own_time: TimeTuple
-    time: TimeTuple
+    _id: int
+    _project_id: int | None
+    _title: str
+    _time: int
 
-    def __init__(self, data: tuple, name: str = "Entry") -> None:
-        super().__init__(name=name)
-        self.task_id = data[0]
-        self.parent_id = data[1]
-        self.name = data[2]
-        self.etype = EntryType.TASK if data[3] == "t" else EntryType.FOLDER
-        self.own_time = TimeTuple(
-            data[4] if data[4] else 0,
-            data[5] if data[5] else 0,
-            data[6] if data[6] else 0,
-        )
-        self.time = self.own_time
+    def __init__(self, data: tuple) -> None:
+        super().__init__()
+        self._id = data[0]
+        self._project_id = data[1]
+        self._title = data[2]
+        self._time = data[3] if data[3] else 0
 
     def on_key(self, event: events.Key) -> None:
         super().on_key(event)
 
-    def reset_own_time(self) -> None:
-        self.own_time = TimeTuple(0, 0, 0)
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def project_id(self) -> int | None:
+        return self._project_id
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @title.setter
+    def title(self, new_title: str) -> None:
+        self._title = new_title
+
+    @property
+    def time(self) -> int:
+        return self._time
+
+    @time.setter
+    def time(self, new_time: int) -> None:
+        self._time = new_time if new_time >= 0 else self._time
+
+    @property
+    def type(self) -> str:
+        return "task" if self._project_id else "project"
 
 
-def generate_empty_entry(task_id: int, parent_id: int, etype: str) -> Entry:
-    return Entry((task_id, parent_id, "", etype, 0, 0, 0))
+def generate_entry(entry_id: int, project_id: int | None) -> Entry:
+    return Entry((entry_id, project_id, "", 0))
