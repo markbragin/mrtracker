b3d27858d6b683edbf07f001b1a32fdf9b581230
delbik <m4rk.brag1n@gmail.com>
2022-08-04 14:43:56 +0300

    create csv

--- mrtracker/backup.py
+++ mrtracker/backup.py
@@ -1,7 +1,9 @@
+import csv
 import os
 import shutil
 
-from .config import DB_PATH, generate_backup_name
+from .config import DB_PATH, generate_backup_name, generate_csv_name
+from .db import fetch_for_csv
 
 
 def create_backup(path: str) -> str:
@@ -12,6 +14,7 @@ def create_backup(path: str) -> str:
         backup_path = path
     else:
         backup_path = os.path.join(".", path)
+
     shutil.copy(DB_PATH, backup_path)
     return os.path.abspath(backup_path)
 
@@ -19,3 +22,30 @@ def create_backup(path: str) -> str:
 def restore_data(path: str) -> None:
     """Replaces all the data with the data from backup"""
     shutil.copy(path, DB_PATH)
+
+
+def create_csv(path: str) -> str:
+    """Creates csv file and returns its location"""
+    if os.path.isdir(path):
+        csv_path = os.path.join(path, generate_csv_name())
+    elif os.path.isdir(os.path.dirname(path)):
+        csv_path = path
+    else:
+        csv_path = os.path.join(".", path)
+
+    data = fetch_for_csv()
+    with open(csv_path, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(
+            (
+                "project",
+                "task",
+                "tags",
+                "date",
+                "start time",
+                "end time",
+                "duration",
+            )
+        )
+        writer.writerows(data)
+    return os.path.abspath(csv_path)
