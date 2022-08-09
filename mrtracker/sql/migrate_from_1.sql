d2a94e2efeceb37d6fc983a71c379d9677d52eab
mark bragin <m4rk.brag1n@gmail.com>
2022-08-09 23:17:43 +0300

    migration from version 1

---
+++ mrtracker/sql/migrate_from_1.sql
@@ -0,0 +1,21 @@
+PRAGMA foreign_keys=ON;
+BEGIN TRANSACTION;
+ALTER TABLE sessions RENAME TO _old_sessions;
+CREATE TABLE sessions(
+    id INTEGER NOT NULL primary key,
+    task_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    start_time TEXT NOT NULL,
+    end_time TEXT NOT NULL,
+    duration INTEGER as (strftime("%s", end_time) - strftime("%s", start_time)) STORED,
+    FOREIGN KEY(task_id)
+        REFERENCES tasks(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+INSERT INTO sessions (id, task_id, date, start_time, end_time)
+SELECT id, task_id, date, start_time, end_time
+FROM _old_sessions;
+DROP TABLE _old_sessions;
+PRAGMA user_version=2;
+COMMIT;
